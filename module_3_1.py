b3656c56961905affa3087bf19e87fa5b7de2c8c
Raoof13 <kakmogu23@gmail.com>
2024-10-02 10:08:10 +0300

    29.09

--- module_3_1.py
+++ module_3_1.py
@@ -1,5 +1,37 @@
-def count_calls():
+from itertools import count
 
-def string_info('n'):
+calls = 0
 
 
+def count_calls(calls):
+    def string_info(string):
+        string = int(len(string)), str(string.upper()), str(string.lower())
+
+        return string
+
+    return count_calls
+    print(string_info('capibara'))
+
+    def is_contains(string, list_to_search):
+        list_to_search = []
+        for i in list(list_to_search):
+            if i == str(string):
+                is_contains = True
+            else:
+                is_contains = False
+
+            return is_contains
+        print(is_contains('urban', ['ban', 'banan', 'urban']))
+        return count_calls
+
+    calls = int(count(string_info())) + int(count(is_contains()))
+
+
+
+
+
+
+print(string_info())
+print(is_contains())
+print(is_contains())
+print(calls)
