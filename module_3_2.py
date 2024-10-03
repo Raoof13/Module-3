23be959dac8f421443757e784fd5e80f0483af10
Raoof13 <kakmogu23@gmail.com>
2024-10-04 00:28:44 +0300

    29.09

--- module_3_2.py
+++ module_3_2.py
@@ -1,8 +1,17 @@
-def send_email(message, recipient, *, sender):
+def send_email(message, recipient, sender = "university.help@gmail.com"):
     for i in recipient:
-        if i != '@' or i != '.com':
+        if any(i not in recipient for i in ['@' or '.com']):
             print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
-
+            return
+        elif recipient == sender:
+            print("Нельзя отправить письмо самому себе!")
+            return
+        elif recipient != sender:
+            print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+            return
+        else:
+            print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+            return
 # Создайте функцию send_email, которая принимает 2 обычных аргумента: message(сообщение), recipient(получатель)
 # и 1 обязательно именованный аргумент со значением по умолчанию sender = "university.help@gmail.com".
 # 1. Если строки recipient or sender не содержит "@" или не оканчивается на ".com"/".ru"/".net",
@@ -15,15 +24,12 @@ def send_email(message, recipient, *, sender):
 #   Здесь <sender> и <recipient> - значения хранящиеся в этих переменных.
 #   За один вызов функции выводится только одно и перечисленных
 # уведомлений! Проверки перечислены по мере выполнения.
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com', sender = "university.help@gmail.com")
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru',
-sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru',
-sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru',
-sender='urban.teacher@mail.ru')
 
-# print("Нельзя отправить письмо самому себе!")
-# print("Письмо успешно отправлено с адреса <sender> на адрес <recipient>.")
-# print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.")
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teachermail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
+
 
