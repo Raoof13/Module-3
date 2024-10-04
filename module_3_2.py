30b37fd6104102e7f456d0855cd12cd19f33186e
Raoof13 <kakmogu23@gmail.com>
2024-10-04 18:39:32 +0300

    29.09

--- module_3_2.py
+++ module_3_2.py
@@ -1,16 +1,17 @@
 def send_email(message, recipient, sender = "university.help@gmail.com"):
-        if ("@" or (".com" or ".ru" or ".net")) not in recipient or ("@" or (".com" or ".ru" or ".net")) not in sender:
-            print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
-            return
-        elif recipient == sender:
-            print("Нельзя отправить письмо самому себе!")
-            return
-        elif recipient != sender:
-            print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
-            return
-        else:
-            print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
-            return
+    domen = ('.com', '.ru', '.net')
+    if '@' not in recipient or '@' not in sender or not sender.endswith(domen) or not recipient.endswith(domen):
+        print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
+        return
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+        return
+    elif recipient != sender != 'urban.info@gmail.com':
+        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+        return
+    else:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+        return
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
