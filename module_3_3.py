8a25005e131aef47d2690f0572648f52018419e9
Raoof13 <kakmogu23@gmail.com>
2024-10-05 08:50:27 +0300

    29.09

---
+++ module_3_3.py
@@ -0,0 +1,15 @@
+
+
+def print_params(a = 1, b = 'строка', c = True):
+    values_list_2 = [54.32, 'Строка', True]
+    values_dict = {a: 1, b: 'Ctroka', c: False}
+
+
+
+print_params(*values_list_2, 42)
+print_params(*values_list_2, 42)
+print_params(*values_list_2, 42)
+print_params(*values_list_2, 42)
+print_params(*values_list_2, 42)
+print_params(b = 25)
+print_params(c = [1,2,3])
