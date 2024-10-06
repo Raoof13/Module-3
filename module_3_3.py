cbb6e6d02ab7f321375525287ee4b0b0ceeab558
Raoof13 <kakmogu23@gmail.com>
2024-10-06 09:23:03 +0300

    29.09

--- module_3_3.py
+++ module_3_3.py
@@ -1,15 +1,15 @@
 #"Распаковка позиционных параметров".
 
 def print_params(a = 1, b = 'строка', c = True):
-    values_list_2 = [54.32, 'Строка', True]
-    values_dict = {a: 1, b: 'Ctroka', c: False}
+    print(a, b, c)
+values_list = [34, 'Список', [1, 2, 3]]
+values_list_2 = [54.32, 'Список2']
+values_dict = {'a': 2, 'b': 'Словарь', 'c': False}
 
 
-
+print_params(*values_list)
+print_params(**values_dict)
 print_params(*values_list_2, 42)
-print_params(*values_list_2, 42)
-print_params(*values_list_2, 42)
-print_params(*values_list_2, 42)
-print_params(*values_list_2, 42)
+print_params()
 print_params(b = 25)
 print_params(c = [1,2,3])
