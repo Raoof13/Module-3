209bac386b8cfc6c261592ae9b2ad7653a8b48c3
Raoof13 <kakmogu23@gmail.com>
2024-10-08 14:02:22 +0300

    29.09

--- module_3_5.py
+++ module_3_5.py
@@ -1,5 +1,6 @@
 
 def get_multiplied_digits(number):
+
     str_number = str(number)
     first = int(str_number[0])
 
@@ -8,6 +9,6 @@ def get_multiplied_digits(number):
 
     return first * get_multiplied_digits(int(str_number[1:]))
 
-result_ = get_multiplied_digits(8)
+result_ = get_multiplied_digits(10205)
 print(result_)
 
