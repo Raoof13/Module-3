83b32eee441adb7e7663653fc756a42f1c5f5635
Raoof13 <kakmogu23@gmail.com>
2024-10-08 10:50:59 +0300

    29.09

--- module_3_4.py
+++ module_3_4.py
@@ -1,13 +1,14 @@
+
+
 def single_root_words(root_word, *other_words):
     same_words = []
-    for i in range(len(other_words)):
-        same_words.append(i)
-        semi_result = root_word.lower() == i.lower()
+    for i in other_words:
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
+            same_words.append(i)
+    return same_words
 
-    return semi_result
 
-
-result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result1 = single_root_words('rich', 'richiest', 'rich', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
 print(result2)
