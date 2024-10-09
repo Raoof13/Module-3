9aacf8f23baed183b4611febddab0c6c78e501c1
Raoof13 <kakmogu23@gmail.com>
2024-10-09 20:28:42 +0300

    29.09

--- module_3_5.py
+++ module_3_5.py
@@ -9,6 +9,6 @@ def get_multiplied_digits(number):
 
     return first * get_multiplied_digits(int(str_number[1:]))
 
-result_ = get_multiplied_digits(123450)
+result_ = get_multiplied_digits(12345)
 print(result_)
 
