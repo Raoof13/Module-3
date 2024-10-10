e0460a238047f7550145946b5e7c35a548fb2924
Raoof13 <kakmogu23@gmail.com>
2024-10-10 06:35:42 +0300

    29.09

--- module_3_5.py
+++ module_3_5.py
@@ -2,13 +2,13 @@
 def get_multiplied_digits(number):
 
     str_number = str(number)
-    first = int(str_number[0])
+    first = int(str_number[0].replace('0', '1'))
 
     if len(str_number) <= 1:
         return first
 
     return first * get_multiplied_digits(int(str_number[1:]))
 
-result_ = get_multiplied_digits(12345)
+result_ = get_multiplied_digits(402030)
 print(result_)
 
